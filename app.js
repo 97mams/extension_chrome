858604c2be3f90540df9e6203ede2fd87411d2a9
97mams <97mams@gmail.com>
2025-07-11 22:27:46 -0700

    fetch api keno

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ btn.addEventListener('click', (e) => {
 })
 
 const fetchData = (formData) => {
-  //
+  const url = "http://localhost:3000/api/keno"
+  fetch(url, {
+    mode:'no-cors',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ keno: formData })
+  })
+  .then(response => response.json()) 
+  .then(data => {
+    console.log('Success:', data)
+  })
 }
